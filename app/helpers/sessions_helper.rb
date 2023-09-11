ef47fb15a8007dfb9efd90320aa004c974d6bd01
Addison Harris <addisonlharris@withoutarival.com>
2023-09-11 19:06:49 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,9 @@ module SessionsHelper
   # logs in the given user
   def log_in(user)
     session[:user_id] = user.id
+    #guard against session replay attacks
+    #see https://bit.ly/33UvK0w for more.
+    session[:session_token] = user.session_token
   end
 
   #remembers a user in a persistent session.
@@ -14,7 +17,10 @@ module SessionsHelper
   #returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -41,5 +47,14 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+  #returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
+
+  #stores the url trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
