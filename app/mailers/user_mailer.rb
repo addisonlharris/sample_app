b182a3e275430ccdb47bcac9024c537d1c452106
Addison Harris <addisonlharris@withoutarival.com>
2023-09-13 21:00:45 +0000

    Add account activation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: user.email, from: "user@realdomain.com", subject: "Account activation"
+    mail to: user.email, from: "addisonlharris@withoutarival.com", subject: "Account activation"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
